9c94df2462481c64be56a7dc3e6f88d5ef38b8a5
Héctor <monroygti2.0@gmail.com>
2025-12-13 18:38:35 +0100

    Add Principal class for area calculation
    Implement main method for area calculator using Rectangulo class.

---
+++ Proyectos/Programacion/CalculadoraAreas/runOnShell/Principal.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+/**
+ * Clase Principal, contiene el metodo main y utiliza los metodos de la clase Rctangulo
+ * @author Héctor Monroy Fuertes
+ * @since 17-11-2025
+ * @version 17-11-2025
+ */
+public class Principal {
+    public static void main (String[]args){
+        Scanner teclado = new Scanner (System.in);
+        Rectangulo forma = new Rectangulo();
+        
+        System.out.println("***********************************************************************");
+        System.out.println("                        CALCULADORA DE AREAS                           ");
+        System.out.println("***********************************************************************");
+        System.out.print("Introduce la medida de la base por favor: ");
+        forma.setBase(teclado.nextFloat());
+        System.out.println("De acuerdo!");
+        System.out.print("Ahora introduce la medida de la alturua por favor: ");
+        forma.setAltura(teclado.nextFloat());
+        forma.rectToString(forma.getBase(), forma.getAltura());
+        System.out.println("El rectangulo tiene un area de " + forma.getArea(forma.getBase(), forma.getAltura()));/* +" y un perimetro de " + forma.getPerimetro(forma.getBase(), forma.getAltura()));*/
+        System.out.println("¿Es un cuadrado? "+ forma.isCuadrado(forma.getBase(), forma.getAltura()));
+    }
+}
